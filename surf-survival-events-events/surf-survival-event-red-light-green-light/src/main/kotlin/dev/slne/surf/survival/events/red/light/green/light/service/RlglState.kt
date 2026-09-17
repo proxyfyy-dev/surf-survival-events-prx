096a7f08fa948bcfd1a0682671784a76051b8640
ProxyFyy <proxyfyy@gmail.com>
2026-09-17 22:30:07 +0200

    Update RlglState.kt
    Unnütze Kommentare entfernt!

--- surf-survival-events-events/surf-survival-event-red-light-green-light/src/main/kotlin/dev/slne/surf/survival/events/red/light/green/light/service/RlglState.kt
+++ surf-survival-events-events/surf-survival-event-red-light-green-light/src/main/kotlin/dev/slne/surf/survival/events/red/light/green/light/service/RlglState.kt
@@ -1,15 +1,8 @@
 package dev.slne.surf.survival.events.red.light.green.light.service
 
 enum class RlglState {
-    /** Round has not been started yet, players may move freely. */
     LOBBY,
-
-    /** Players may move. */
     GREEN,
-
-    /** Any movement gets a player eliminated. */
     RED,
-
-    /** Round is over, no more eliminations/finishes are processed. */
     FINISHED
 }
